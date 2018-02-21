ff03b9c036ae137fe1028b4bcc54190cb9ad9340
derrkater <k.m.trojanowski@gmail.com>
2018-02-21 20:06:57 +0100

    Implement data_statistics and utility functions.

--- utility/data_statistics.py
+++ utility/data_statistics.py
@@ -1,14 +1,62 @@
+from collections import defaultdict
+import numpy as np
+
+
 def get_average_pilot_count(lists):
+    pilot_counts = []
+    for list in lists:
+        pilot_counts.append(len(list['pilots']))
+
+    return np.average(pilot_counts)
+
+
+def get_pilot_counts_histogram(lists):
     raise NotImplementedError
 
 
 def get_average_upgrade_count(lists):
+    upgrades_counts = []
+    for list in lists:
+        upgrades_count = 0
+        for pilot in list['pilots']:
+            upgrades_by_type = pilot.get('upgrades', {})
+            upgrades_count += sum(len(upgrades_by_type) for upgrades_by_type in upgrades_by_type.values())
+        upgrades_counts.append(upgrades_count)
+
+    return np.average(upgrades_counts)
+
+
+def get_upgrade_count_histogram(lists):
     raise NotImplementedError
 
 
 def get_average_upgrade_count_per_ship(lists):
+    upgrades_counts = []
+    for list in lists:
+        for pilot in list['pilots']:
+            upgrades_by_type = pilot.get('upgrades', {})
+            upgrades_count = sum(len(upgrades_by_type) for upgrades_by_type in upgrades_by_type.values())
+            upgrades_counts.append(upgrades_count)
+
+    return np.average(upgrades_counts)
+
+
+
+def get_upgrade_count_per_ship_histogram(lists):
     raise NotImplementedError
 
 
 def get_average_points(lists):
-    raise NotImplementedError
+    point_costs = []
+    for list in lists:
+        point_costs.append(list['points'])
+
+    return np.average(point_costs)
+
+
+def get_faction_count(lists):
+    factions_count = defaultdict(lambda: 0)
+    for list in lists:
+        factions_count[list['faction']] += 1
+
+    return factions_count
--- utility/lists_utility.py
+++ utility/lists_utility.py
@@ -1,13 +1,26 @@
 def get_lists(matches):
     lists = []
     for match in matches:
-        list_1 = match['player1']['list']
-        if list_1: lists.append(list_1)
+        player_1 = match['player1']
+        if player_1['list']:
+            list_1 = player_1['xws']
+            list_1['points'] = _get_points_from_pretty_print(player_1)
+            lists.append(list_1)
 
-        list_2 = match['player2']['list']
-        if list_2: lists.append(list_2)
+        player_2 = match['player2']
+        if player_2['list']:
+            list_2 = player_2['xws']
+            list_2['points'] = _get_points_from_pretty_print(player_2)
+            lists.append(list_2)
 
     return lists
+
+
+def _get_points_from_pretty_print(player):
+    pretty_print = player['pretty_print']
+    point_cost = pretty_print.split('\n')[-1][1:-1]
+
+    return int(point_cost)
 
 
 def count_lists(matches):
